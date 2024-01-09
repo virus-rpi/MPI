fe383d57de1c4159d63fab3975d77981c147635f
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2024-01-09 23:00:47 +0100

    feat: add webui

--- src/main/java/com/virusrpi/mpi/modules/API.java
+++ src/main/java/com/virusrpi/mpi/modules/API.java
@@ -151,9 +151,23 @@ public class API {
 			return Client.INSTANCE.isHeadless();
 		});
 
-		get("/", (request, response) -> {
+		get("/getCurrentAddress", (request, response) -> {
 			response.status(200);
-			return generateHtml(Client.INSTANCE.isAutoReconnect(), Client.INSTANCE.isAutoRespawn(), Client.INSTANCE.isHeadless());
+			return address.isEmpty() ? "Not connected" : address;
+		});
+
+		get("/", (request, response) -> {
+			String disconnectMessage = "";
+			if (mc.currentScreen instanceof DisconnectedScreen) {
+				response.status(200);
+				disconnectMessage = reason.getString();
+			} else {
+				response.status(400);
+				disconnectMessage = "Not on a DisconnectedScreen";
+			}
+			response.status(200);
+            assert mc.currentScreen != null;
+            return generateHtml(address, mc.currentScreen.toString(), disconnectMessage, Client.INSTANCE.isAutoReconnect(), Client.INSTANCE.isAutoRespawn(), Client.INSTANCE.isHeadless());
 		});
 	}
 
@@ -183,10 +197,12 @@ public class API {
 
 	public void resetConnect() { connect = false; }
 
-	private String generateHtml(boolean autoReconnect, boolean autoRespawn, boolean headless) {
+	private String generateHtml(String address, String currentScreen, String disconnectReason, boolean autoReconnect, boolean autoRespawn, boolean headless) {
 		String autoReconnectColor = autoReconnect ? "green" : "red";
 		String autoRespawnColor = autoRespawn ? "green" : "red";
 		String headlessColor = headless ? "green" : "red";
+
+		address = address.isEmpty() ? "Not connected" : address;
 
 		String html = """
             <!DOCTYPE html>
@@ -194,7 +210,6 @@ public class API {
                 <head>
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width">
-                    <meta http-equiv="refresh" content="5">
                     <title>MPI WebUI</title>
                     <style>
                         body {
@@ -202,6 +217,8 @@ public class API {
                             background-color: #1e1e1e;
                             color: #fff;
                             padding: 0 20px;
+                            padding-left: 5%%;
+                            font-size: 1.5em;
                         }
                         h1 {
                             text-align: center;
@@ -236,6 +253,17 @@ public class API {
                             cursor: pointer;
                             border-radius: 10px;
                         }
+                        input {
+                            width: 200px;
+                            padding: 12px 20px;
+                            margin: 8px 0;
+                            box-sizing: border-box;
+                            border: 2px solid #ccc;
+                            border-radius: 4px;
+                        }
+                        .cursive {
+                            font-style: italic;
+                        }
                     </style>
                     <script>
                         function setAutoReconnect(autoReconnect) {
@@ -264,26 +292,107 @@ public class API {
                                     document.getElementById('headlessCircle').className = headless === 'true' ? 'circle green' : 'circle red';
                                 });
                             }
+                            
+                            function connect() {
+                                let ip = document.getElementById('ip').value;
+                                let port = document.getElementById('port').value;
+                                if (port === '') {
+                                    port = '25565';
+                                }
+                                if (ip === '') {
+                                    return;
+                                }
+                                fetch('/connect?ip=' + ip + '&port=' + port);
+                                
+                                refresh();
+                            }
+                            
+                            function disconnect() {
+                                fetch('/disconnect');
+                            }
+						
+                            function escapeDisconnect() {
+                                 fetch('/escapeDisconnect');
+                            }
+                            
+                            function refresh() {
+                                 fetch('/getAutoReconnect')
+                                 .then(response => response.text())
+                                 .then(autoReconnect => {
+                                     document.getElementById('autoReconnectCircle').className = autoReconnect === 'true' ? 'circle green' : 'circle red';
+                                 });
+				                
+                                 fetch('/getAutoRespawn')
+                                 .then(response => response.text())
+                                 .then(autoRespawn => {
+                                     document.getElementById('autoRespawnCircle').className = autoRespawn === 'true' ? 'circle green' : 'circle red';
+                                 });
+				                
+                                 fetch('/getHeadless')
+                                 .then(response => response.text())
+                                 .then(headless => {
+                                     document.getElementById('headlessCircle').className = headless === 'true' ? 'circle green' : 'circle red';
+                                 });
+                                 
+                                 fetch('/getScreen')
+                                 .then(response => response.text())
+                                 .then(screen => {
+                                     document.getElementById('screen').innerHTML = screen;
+                                 });
+                                 
+                                 fetch('/getCurrentAddress')
+                                 .then(response => response.text())
+                                 .then(address => {
+                                     document.getElementById('address').innerHTML = address;
+                                 });
+								 
+                                 fetch('/getDisconnectReason')
+                                 .then(response => response.text())
+                                 .then(disconnectReason => {
+                                     document.getElementById('disconnect_reason').innerHTML = disconnectReason;
+                                 });
+                            }
                     </script>
                 </head>
                 <body>
                     <h1>MPI WebUI</h1>
+                    
+                    <button onclick="refresh()">Refresh</button>
+                    </br>
+                    
+                    <p>Current address: <span id="address" class="cursive">%s</span></p>
+                    <p>Current screen: <span id="screen" class="cursive">%s</span></p>
+                    <p>Current disconnect reason: <span id="disconnect_reason" class="cursive">%s</span></p>
+                    
                     <p>Auto reconnect:</p>
                     <div id='autoReconnectCircle' class='circle %s'></div>
                     <button onclick="setAutoReconnect(true)">On</button>
                     <button onclick="setAutoReconnect(false)">Off</button>
+                    
                     <p>Auto respawn:</p>
                     <div id='autoRespawnCircle' class='circle %s'></div>
                     <button onclick="setAutoRespawn(true)">On</button>
-                     <button onclick="setAutoRespawn(false)">Off</button>
-                     <p>Headless:</p>
-                     <div id='headlessCircle' class='circle %s'></div>
-                     <button onclick="setHeadless(true)">On</button>
-                     <button onclick="setHeadless(false)">Off</button>
+                    <button onclick="setAutoRespawn(false)">Off</button>
+                    
+                    <p>Headless:</p>
+                    <div id='headlessCircle' class='circle %s'></div>
+                    <button onclick="setHeadless(true)">On</button>
+                    <button onclick="setHeadless(false)">Off</button>
+                    
+                    <p>Connect:</p>
+                    <input id="ip" type="text" placeholder="IP">
+                    <input id="port" type="text" placeholder="Port">
+                    <button onclick="connect()">Connect</button>
+				
+                    <p>Disconnect:</p>
+                    <button onclick="disconnect()">Disconnect</button>
+				
+                    <p>Escape Disconnect:</p>
+                    <button onclick="escapeDisconnect()">Escape Disconnect</button>
                 </body>
             </html>
             """;
 
-		return String.format(html, autoReconnectColor, autoRespawnColor, headlessColor);
+		return String.format(html, address, currentScreen, disconnectReason, autoReconnectColor, autoRespawnColor, headlessColor);
 	}
 }
